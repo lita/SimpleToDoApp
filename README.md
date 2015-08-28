4c4a873568cc3426144e97b10a176d5181ef3c20
Lita Cho <lita.cho@gmail.com>
2015-08-27 22:53:35 -0700

    inital app

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple ToDo App for Android
+
+![alt tag](https://raw.github.com/lita/SimpleToDo/master/CodePath_Assignment.gif)
+
+### Features
+
+* Able to add to-do items
+* Able to remove to-do items with a long click (pressing on the item for 2 seconds)
+* Able to edit to-do items by clicking on the item.
+    * The edit page focuses on the edit text box.
+    * Old value is propagated into the edit text box and the cursor starts at the end.
+* Will persist the data entered into the app.
+
---
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -0,0 +1,125 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+    public final static String EDIT_ITEM_VALUE = "com.codepath.simpletodo.MESSAGE";
+    public final static String EDIT_ITEM_POS = "com.codepath.simpletodo.ITEM_POS";
+    private final int REQUEST_CODE = 20;
+    public final static int RESULT_OK = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter,
+                                           View item, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter,
+                                    View item, int pos, long id) {
+                Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                String itemText = items.get(pos);
+                intent.putExtra(EDIT_ITEM_VALUE, itemText);
+                intent.putExtra(EDIT_ITEM_POS, pos);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            String itemText = data.getExtras().getString(EDIT_ITEM_VALUE);
+            int pos = data.getExtras().getInt(EDIT_ITEM_POS, 0);
+            items.set(pos, itemText);
+            writeItems();
+            itemsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
